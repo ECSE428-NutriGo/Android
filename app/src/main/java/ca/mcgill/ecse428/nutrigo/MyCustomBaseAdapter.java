73a03280b3235e433015d1fa042f3aa29664f0fb
willbouch <william.bouchard3@mail.mcgill.ca>
2020-02-15 11:23:10 -0500

    Done with my 2 pages

---
+++ app/src/main/java/ca/mcgill/ecse428/nutrigo/MyCustomBaseAdapter.java
@@ -0,0 +1,57 @@
+package ca.mcgill.ecse428.nutrigo;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MyCustomBaseAdapter extends BaseAdapter {
+    private static ArrayList<ListItem> searchArrayList;
+
+    private LayoutInflater mInflater;
+
+    public MyCustomBaseAdapter(Context context, ArrayList<ListItem> results) {
+        searchArrayList = results;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public int getCount() {
+        return searchArrayList.size();
+    }
+
+    public Object getItem(int position) {
+        return searchArrayList.get(position);
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.multi_lines, null);
+            holder = new ViewHolder();
+            holder.txtName = (TextView) convertView.findViewById(R.id.name);
+            holder.txtCityState = (TextView) convertView.findViewById(R.id.items);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.txtName.setText(searchArrayList.get(position).getName());
+        holder.txtCityState.setText(searchArrayList.get(position).getItems());
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView txtName;
+        TextView txtCityState;
+    }
+}
