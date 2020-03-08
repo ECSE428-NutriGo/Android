fe2386f5c8cc6d064e2fc7acbacae57b9ac93d22
AleksasMurauskas <aleksmurauskas@gmail.com>
2020-03-08 01:15:30 -0500

    Base before testing

--- app/src/main/java/ca/mcgill/ecse428/nutrigo/ChangeContactInfoActivity.java
+++ app/src/main/java/ca/mcgill/ecse428/nutrigo/ChangeContactInfoActivity.java
@@ -1,22 +1,81 @@
 package ca.mcgill.ecse428.nutrigo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ChangeContactInfoActivity extends AppCompatActivity {
-
+    private String oldpass;
+    private String username;
     private final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_contact_info);
+        final EditText edit_username_field = findViewById(R.id.edit_username_field);
+
+        RequestParams rp = new RequestParams();
+        asyncHttpClient.addHeader("Authorization", "Token "+LoginActivity.getUserToken());
+        asyncHttpClient.get("https://nutrigo-staging.herokuapp.com/rest-auth/user/", rp, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    edit_username_field.setText(response.get("username").toString());
+                    oldpass= response.get("password").toString();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText( ChangeContactInfoActivity.this, errorResponse.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    public void change(){
+        final EditText edit_username_field = findViewById(R.id.edit_username_field);
+        final EditText edit_curr_field = findViewById(R.id.edit_current_pass_field);
+        final EditText edit_new_pass_field = findViewById(R.id.edit_new_pass_field);
+        final EditText edit_new_pass2_field = findViewById(R.id.edit_new_pass2_field);
 
+        if(!edit_new_pass_field.getText().equals(edit_new_pass2_field.getText())){
+            return;
+        }
+        if(!edit_curr_field.getText().equals(oldpass)){
+            return;
+        }
+
+        RequestParams rp = new RequestParams();
+        rp.put("username",edit_username_field.getText());
+        rp.put("password",edit_new_pass_field.getText());
+        asyncHttpClient.addHeader("Authorization", "Token "+LoginActivity.getUserToken());
+        asyncHttpClient.put("https://nutrigo-staging.herokuapp.com/rest-auth/user/", rp, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Intent ide = new Intent(ChangeContactInfoActivity.this, MainActivity.class);
+                startActivity(ide);
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText( ChangeContactInfoActivity.this, errorResponse.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
 
 
--- app/src/main/java/ca/mcgill/ecse428/nutrigo/EditUserActivity.java
+++ app/src/main/java/ca/mcgill/ecse428/nutrigo/EditUserActivity.java
@@ -31,47 +31,58 @@ public class EditUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
-        final EditText edit_age_field = findViewById(R.id.edit_age_field);
+        final EditText edit_protein_field = findViewById(R.id.edit_protein_field);
+        final EditText edit_carb_field = findViewById(R.id.edit_carb_field);
+        final EditText edit_fat_field = findViewById(R.id.edit_fat_field);
         final EditText edit_current_weight_field = findViewById(R.id.edit_current_weight_field);
         final EditText edit_target_weight_field = findViewById(R.id.edit_target_weight_field);
+        final EditText edit_age_field = findViewById(R.id.edit_age_field);
+        final EditText edit_hours_field = findViewById(R.id.edit_hours_field);
         RequestParams rp = new RequestParams();
         asyncHttpClient.addHeader("Authorization", "Token "+LoginActivity.getUserToken());
         asyncHttpClient.get("https://nutrigo-staging.herokuapp.com/rest-auth/user/", rp, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                try {
+                   edit_protein_field.setText(response.get("protein_target").toString());
+                   edit_carb_field.setText(response.get("carb_target").toString());
+                   edit_fat_field.setText(response.get("fat_target").toString());
+                   edit_current_weight_field.setText(response.get("current_weight").toString());
+                   edit_target_weight_field.setText(response.get("target_weight").toString());
                    edit_age_field.setText(response.get("age").toString());
-                   edit_current_weight_field.setText(response.get("").toString());
-                   edit_age_field.setText(response.get("age").toString());
+                   edit_hours_field.setText(response.get("hours_activity").toString());
                } catch (JSONException e) {
                    e.printStackTrace();
                }
 
             }
-
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 Toast.makeText(EditUserActivity.this, errorResponse.toString(), Toast.LENGTH_LONG).show();
             }
         });
+
     }
 
 
     public void edit(View view){
-        final EditText edit_age_field = findViewById(R.id.edit_age_field);
+        final EditText edit_protein_field = findViewById(R.id.edit_protein_field);
+        final EditText edit_carb_field = findViewById(R.id.edit_carb_field);
+        final EditText edit_fat_field = findViewById(R.id.edit_fat_field);
         final EditText edit_current_weight_field = findViewById(R.id.edit_current_weight_field);
         final EditText edit_target_weight_field = findViewById(R.id.edit_target_weight_field);
+        final EditText edit_age_field = findViewById(R.id.edit_age_field);
+        final EditText edit_hours_field = findViewById(R.id.edit_hours_field);
 
-
+        //Create Request
         RequestParams rp = new RequestParams();
+        rp.put("protein_target",edit_protein_field.getText());
+        rp.put("carb_target",edit_carb_field.getText());
+        rp.put("fat_target",edit_fat_field.getText());
+        rp.put("current_weight", edit_current_weight_field.getText());
+        rp.put("target_weight", edit_target_weight_field.getText());
         rp.put("age", edit_age_field.getText());
-        rp.put("currentweight", edit_current_weight_field.getText());
-        rp.put("targetweight", edit_target_weight_field.getText());
-
-        //age
-
-        //Edit Target weight
-        //Current Weight
+        rp.put("hours_activity", edit_hours_field.getText());
 
         asyncHttpClient.addHeader("Authorization", "Token "+LoginActivity.getUserToken());
         asyncHttpClient.put("https://nutrigo-staging.herokuapp.com/rest-auth/user/", rp, new JsonHttpResponseHandler() {
